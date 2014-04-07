8d70487b4c2a0bf510d7f068c6b60e2363580edc
haetze <richy.sting@gmail.com>
2014-04-07 19:51:08 +0200

    fix

--- parse.go
+++ parse.go
@@ -9,11 +9,11 @@ import (
 func GetDataFieldFromFile(file, field string) []string {
 	bytesStr, _ := ioutil.ReadFile(file)
 	str := string(bytesStr)
-	mes := strings.SplitAfter(str, field + "\"" )
+	mes := strings.SplitAfter(str, field + "=\"" )
 	var data []string
-	for n, m := range mes{
+	for _, m := range mes{
 		b := strings.SplitAfter(m, "\"")
-		data[n] = strings.Replace(b[0], "\"", " ", 1)
+		data = append(data, strings.Replace(b[0], "\"", " ", 1))
 
 	}
 	return data
